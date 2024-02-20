870dfc60e2d2d7a6a6592b5e45e6cd6c889e4a50
sushantnair <sushantnair.developer@gmail.com>
2024-02-20 17:59:59 +0530

    Create signup_modal.php

---
+++ includes/signup_modal.php
@@ -0,0 +1,36 @@
+<div class = "modal fade" id = "signup-modal" tabindex = "-1" role = "dialog">
+    <div class="modal-dialog" role="document">  
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="signup-heading">Signup with KJSCE CFAS</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <nav class = "navbar navbar-expand-md navbar-light">
+                    <div class = "collapse navbar-collapse justify-content-center" id = "my-navbar">
+                        <ul class = "navbar-nav">
+                            <li class = "nav-item">
+                                <a class = "nav-link" href = "#" data-toggle = "modal" data-target = "#student-signup-modal">
+                                    <button class = "btn btn-primary"><i class = "fas fa-user"></i>&nbsp;&nbsp;Signup as Student</button>
+                                </a>
+                            </li>
+                            <div class = "nav-vl"></div>
+                            <li class = "nav-item">
+                                <a class = "nav-link" href = "#" data-toggle = "modal" data-target = "#faculty-signup-modal">
+                                    <button class = "btn btn-primary"><i class = "fas fa-sign-in-alt"></i>&nbsp;&nbsp;Signup as Faculty</button>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav> 
+            </div>
+            <div class = "modal-footer">
+                <span>Account already created?<span>
+                <span><a href = "#" data-dismiss = "modal" data-toggle = "modal" data-target = "#student-login-modal">Login as Student</a></span>
+                <span><a href = "#" data-dismiss = "modal" data-toggle = "modal" data-target = "#faculty-login-modal">Login as Faculty</a></span>
+            </div>
+        </div>  
+    </div>
+</div>
